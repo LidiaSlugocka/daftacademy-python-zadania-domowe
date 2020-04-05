692dee7afeb39678394da0430637b5ad399001e0
LidiaSlugocka <lidia.slugocka.stud@pw.edu.pl>
2020-04-05 17:56:53 +0200

    Zadanie 2 poprawione

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from typing import Dict
+
 from fastapi import FastAPI
 
 from pydantic import BaseModel
 
+
 app = FastAPI()
+
 
 # Zadanie 1
 @app.get("/")
@@ -10,21 +14,19 @@ def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 # Zadanie 2
-class MethodResp(BaseModel):
+#class GiveMeSomethingRq(BaseModel):
+#    first_key: str
+
+
+class GiveMeSomethingResp(BaseModel):
     method: str
+    #received: Dict
+    #constant_data: str = "python jest super"
 
-@app.get('/{method}', response_model=MethodResp)
-def print_method(method: str):
-    return MethodResp(method=f"{method}")
+@app.get("/{method}", response_model=GiveMeSomethingResp)
+def receive_something(method: str):
+    return GiveMeSomethingResp(method=f'{method}')
 
-@app.post('/{method}', response_model=MethodResp)
-def print_method(method: str):
-    return MethodResp(method=f"{method}")
-
-@app.put('/{method}', response_model=MethodResp)
-def print_method(method: str):
-    return MethodResp(method=f"{method}")
-
-@app.delete('/{method}', response_model=MethodResp)
-def print_method(method: str):
-    return MethodResp(method=f"{method}")
+@app.post("/{method}", response_model=GiveMeSomethingResp)
+def receive_something(method: str):
+    return GiveMeSomethingResp(method=f'{method}')
