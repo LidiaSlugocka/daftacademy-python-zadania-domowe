0f742567559baa69670ee75deb03cc1f326b6ec8
LidiaSlugocka <lidia.slugocka.stud@pw.edu.pl>
2020-04-05 15:31:50 +0200

    Zadanie pierwsze gotowe

--- D_jak_deploy/main.py
+++ D_jak_deploy/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
 
-from pydantic import BaseModel
-
 app = FastAPI()
 
 @app.get("/")
 def hello_world():
-    return {"message": "Hello world"}
-
-class HelloNameResp(BaseModel):
-    message: str
-
-@app.get('/hello/{name}', response_model=HelloNameResp) #dynamiczne adresy
-def hello_name(name: str):
-    return HelloNameResp(message=f"Hello {name}")
+    return {"message": "Hello World during the coronavirus pandemic!"}
