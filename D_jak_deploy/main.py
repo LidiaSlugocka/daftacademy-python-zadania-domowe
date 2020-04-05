59432f715d194b246152eda3234f6a923bbc995f
LidiaSlugocka <lidia.slugocka.stud@pw.edu.pl>
2020-04-05 15:44:53 +0200

    Zadanie drugie dodane

--- D_jak_deploy/main.py
+++ D_jak_deploy/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 @app.get("/")
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+class MethodResp(BaseModel):
+    method: str
+
+@app.get('/{method}', response_model=MethodResp) #dynamiczne adresy
+def print_method(method: str):
+    return MethodResp(method=f"{method}")
