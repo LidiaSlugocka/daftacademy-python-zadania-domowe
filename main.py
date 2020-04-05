3a2801afa5354359228fab82d2d6576362645365
LidiaSlugocka <lidia.slugocka.stud@pw.edu.pl>
2020-04-05 16:24:03 +0200

    Zadanie 2 poprawione

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# Zadanie 1
 @app.get("/")
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
+# Zadanie 2
 class MethodResp(BaseModel):
     method: str
 
-@app.get('/{method}', response_model=MethodResp) #dynamiczne adresy
+@app.get('/{method}', response_model=MethodResp)
+def print_method(method: str):
+    return MethodResp(method=f"{method}")
+
+@app.post('/{method}', response_model=MethodResp)
+def print_method(method: str):
+    return MethodResp(method=f"{method}")
+
+@app.put('/{method}', response_model=MethodResp)
+def print_method(method: str):
+    return MethodResp(method=f"{method}")
+
+@app.delete('/{method}', response_model=MethodResp)
 def print_method(method: str):
     return MethodResp(method=f"{method}")
