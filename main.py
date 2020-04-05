bef177fab6b35c4c465e659826cd07be5709043f
LidiaSlugocka <lidia.slugocka.stud@pw.edu.pl>
2020-04-05 21:03:43 +0200

    Zadanie 3 dodane

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 
 from pydantic import BaseModel
 
+import json
+
 
 app = FastAPI()
-
+app.counter = -1
 
 # Zadanie 1
 @app.get("/")
@@ -32,3 +34,19 @@ def put_method(method: str):
 @app.delete("/{method}", response_model=MethodReturn)
 def delete_method(method: str):
     return MethodReturn(method='DELETE')
+
+# Zadanie 3
+class PatientBasicData(BaseModel):
+    name: str
+    surname: str
+
+class PatientData(BaseModel):
+    id: int
+    patient: PatientBasicData
+    
+@app.post('/patient', response_model=PatientBasicData)
+def new_patient(patient: PatientBasicData):
+    app.counter += 1
+    return PatientData(id=app.counter, patient=data)
+
+# Zadanie 4
--- test_main.py
+++ test_main.py
@@ -6,11 +6,13 @@ import pytest
 
 client = TestClient(app)
 
+# Test zadania 1
 def test_hello_world():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
 
+# Test zadania 2
 @pytest.mark.parametrize("method", ['Zenek', 'Marek', 'Ziemniak'])
 def test_get_method(method):
     response = client.get(f"/{method}")
@@ -34,3 +36,13 @@ def test_delete_method(method):
     response = client.delete(f"/{method}")
     assert response.status_code == 200
     assert response.json() == {"method": "DELETE"}
+
+# Test zadania 3
+#patient = PatientBasicData(name="Lidia", surname="Slugocka")
+#@pytest.mark.parametrize("params", ['{"name": "Lidia", "surname": "Sługocka"}'])
+#def test_new_patient(patient):
+#    response = client.post("/patient")
+#    assert responsse.status_code == 200
+#    assert response.json() == {"id": 0, "patient": params}
+
+# Test zadania 4
