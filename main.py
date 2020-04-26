b89969226fc47ded02681775a2eb44975005b2b4
LidiaSlugocka <lidia.slugocka.stud@pw.edu.pl>
2020-04-26 23:31:57 +0200

    Excercise 2 first try

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Response, HTTPException, Depends, Cookie, status
 from pydantic import BaseModel
+from hashlib import sha256
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.responses import RedirectResponse
 
+import secrets
 
 app = FastAPI()
 app.counter = -1
@@ -54,3 +58,26 @@ def find_patient(pk: int):
         return app.patients[pk]
     else:
         raise HTTPException(204,"No such patient!")
+
+############################################################################ F_jak_ficzur
+# Zadanie 2
+my_user = {"trudnY": "paC13Nt"}
+app.secret_key = "it is only my secret key and noone knows it"
+app.tokens = []
+security = HTTPBasic()
+
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    my_username = secrets.compare_digest(credentials.username, "trudnY")
+    my_password = secrets.compare_digest(credentials.password, "paC13Nt")
+    if my_username and my_password:
+        session_token = sha256(bytes(f"{user}{password}{app.secret}", encoding="utf8")).hexdigest()
+        app.tokens += session_token
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return credentials.username
+
+app.post("/login")
+def log_into(response: Response, user_credentials = Depends(get_current_user)):
+        response.set_cookie(key="session_token", value=session_token)
+        response = RedirectResponse(url="/welcome")
+        return response
